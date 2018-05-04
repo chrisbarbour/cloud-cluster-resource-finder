cd9cd9c34bd731224abafea2abe1bb0b1aa00d2b
chrisbarbour <bobjamin@live.co.uk>
2018-05-04 17:59:21 +0100

    fixing api to return info about status

--- src/main/kotlin/com/cluster/ApiReactor.kt
+++ src/main/kotlin/com/cluster/ApiReactor.kt
@@ -10,25 +10,47 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.DeleteObjectsRequest
+import com.amazonaws.services.securitytoken.AWSSecurityTokenService
+import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClient
+import com.amazonaws.services.securitytoken.model.AssumeRoleRequest
 import com.amazonaws.services.sns.AmazonSNS
 import com.amazonaws.services.sns.AmazonSNSClient
+import java.util.*
 
 class ApiReactor(
         val s3Client: AmazonS3 = AwsConfigurator.defaultClient(AmazonS3Client.builder()),
         val snsClient: AmazonSNS = AwsConfigurator.defaultClient(AmazonSNSClient.builder()),
         val iotClient: AWSIot = AwsConfigurator.defaultClient(AWSIotClient.builder()),
+        val stsClient: AWSSecurityTokenService = AwsConfigurator.defaultClient(AWSSecurityTokenServiceClient.builder()),
         val resourceBucket: String = System.getenv("BUCKET"),
-        val loadTopic: String = System.getenv("LOAD_TOPIC")
+        val loadTopic: String = System.getenv("LOAD_TOPIC"),
+        val iotRoleArn: String = System.getenv("IOT_ROLE_ARN")
 ): RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     override fun handleRequest(apiGatewayProxyRequestEvent: APIGatewayProxyRequestEvent, context: Context): APIGatewayProxyResponseEvent {
         val response = when(apiGatewayProxyRequestEvent.path){
             "/reset" -> handleReset()
             "/status" -> handleStatus()
             "/load" -> handleLoad()
+            "/iotAccess" -> iotAccess()
             "/notificationEndpoint" -> handleNotificationEndpoint()
             else -> 400 to "NotFound"
         }
-        return APIGatewayProxyResponseEvent().withStatusCode(response.first).withBody(response.second)
+        return APIGatewayProxyResponseEvent()
+                .withStatusCode(response.first)
+                .withBody(response.second)
+                .withHeaders(mapOf(
+                        "Access-Control-Allow-Origin" to "*"
+                ))
+    }
+
+    fun iotAccess(): Pair<Int, String>{
+        val credentials = stsClient.assumeRole(
+                AssumeRoleRequest()
+                        .withRoleArn(iotRoleArn)
+                        .withDurationSeconds(3600)
+                        .withRoleSessionName(UUID.randomUUID().toString())
+        ).credentials
+        return 200 to "{\"AWS_ACCESS_KEY_ID\":\"${credentials.accessKeyId}\", \"AWS_SECRET_ACCESS_KEY\":\"${credentials.secretAccessKey}\", \"AWS_SESSION_TOKEN\":\"${credentials.sessionToken}\"}"
     }
 
     fun handleNotificationEndpoint(): Pair<Int, String>{
@@ -56,13 +78,9 @@ class ApiReactor(
     fun handleStatus(): Pair<Int, String>{
         val keys = s3Client.listObjects(resourceBucket).objectSummaries.map { it.key }
         val status = (if(keys.isEmpty()) "Not" else "") + "Started"
-        val directories = if(keys.isNotEmpty()) {
-            keys
-            .filter { it.endsWith('/') }
-            .map { it.substringBeforeLast('/') }
-            .fold(StringBuilder()) { acc, it -> acc.append("\"$it\", ") }
-            .toString().substringBeforeLast(',')
-        } else ""
-        return 200 to "{\"status\":\"$status\", \"keys\":[$directories]}"
+        fun listString(list: Collection<*>) = list.foldIndexed(StringBuilder()){i, acc, it -> acc.append("\"$it\"").append(if(i<list.size-1){", "}else {""})}.toString()
+        val loaded = keys.map{ it.substringBefore('/')}.toSet()
+        val toLoad = listOf("lambda", "ec2", "ecs", "dynamodb").filter { !loaded.contains(it) }
+        return 200 to "{\"status\":\"$status\", \"loaded\":[${listString(loaded)}], \"waitingOn\":[${listString(toLoad)}]}"
     }
 }
