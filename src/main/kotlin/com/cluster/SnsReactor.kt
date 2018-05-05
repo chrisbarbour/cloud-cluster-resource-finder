d7f2e71194dca2b902ebc7a46dd2f1356564563a
chrisbarbour <bobjamin@live.co.uk>
2018-05-05 11:17:34 +0100

    closes #7, added lambda info

--- src/main/kotlin/com/cluster/SnsReactor.kt
+++ src/main/kotlin/com/cluster/SnsReactor.kt
@@ -6,11 +6,13 @@ import com.amazonaws.services.lambda.runtime.events.SNSEvent
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.ObjectMetadata
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import java.io.ByteArrayInputStream
 
 class SnsReactor(
         val s3Client: AmazonS3 = AwsConfigurator.defaultClient(AmazonS3Client.builder()),
         val resourceBucket: String = System.getenv("BUCKET"),
+        val accountId: String = System.getenv("ACCOUNT"),
         val resourceType: String = System.getenv("RESOURCE"),
         val iotPublisher: IotPublisher = IotPublisher()
 ): RequestHandler<SNSEvent, String> {
@@ -23,9 +25,13 @@ class SnsReactor(
     }
 
     fun lambda(){
-        val lambdas = AwsResourceFinderLambda().lambdaResources("eu-west-1", "account").map { it.resource.arn }
-        val lambdaInfo = lambdas.fold(StringBuilder()){acc, it -> acc.append(it).append("\n")}.toString()
-        s3Client.putObject(resourceBucket,resourceType + "/info.json",ByteArrayInputStream(lambdaInfo.toByteArray()), ObjectMetadata())
+        val lambdas = AwsResourceFinderLambda().lambdaResources("eu-west-1", accountId)
+        s3Client.putObject(
+                resourceBucket,
+                resourceType + "/info.json",
+                ByteArrayInputStream(jsonWriter.writeValueAsBytes(lambdas)),
+                ObjectMetadata()
+        )
         iotPublisher.postEvent(resourceType)
     }
 
@@ -36,4 +42,7 @@ class SnsReactor(
         iotPublisher.postEvent(resourceType)
     }
 
+    companion object {
+        val jsonWriter = jacksonObjectMapper().writerWithDefaultPrettyPrinter()
+    }
 }
